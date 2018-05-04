0cb215202a852c4fa7946eead2e3e29b5a577747
Administrator <Administrator@PC-20180408CECO>
2018-05-04 09:59:46 +0800

    two commit

---
+++ MyBlog/src/main/java/com/youzi/MyBlog/MainApplication.java
@@ -0,0 +1,31 @@
+package com.youzi.MyBlog;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@MapperScan("com.youzi.DSSession.dao")
+public class MainApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+		return new DataSource();
+	}
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource());
+		return sqlSessionFactoryBean.getObject();
+	}
+}
